ed6c6760edbe12142a3d05d9fbfe57ebf8326339
BeyondOwn <beyondown@gmail.com>
2025-03-15 23:25:55 +0200

    vue-project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import './globals.css'
+</script>
+
+<template>
+  <div class="dark bg-background text-foreground">
+    <header class="h-[20px]">
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const text = ref('')
+
+const mock = [
+  'heyaa',
+  'bruv',
+  're',
+  'Etc',
+  'heyaa',
+  'bruv',
+  're',
+  'Etc',
+  'heyaa',
+  'bruv',
+  're',
+  'Etc',
+  'heyaa',
+  'bruv',
+  're',
+  'Etc',
+]
+</script>
+
+<template>
+  <main class="min-h-[calc(100vh-20px)] border-1 border-red-500 grid grid-rows-6 grid-cols-6">
+    <!-- Messages -->
+    <div class="col-span-5 row-span-5 border-1 border-blue-500">
+      <div class="pl-1 flex flex-col" v-for="(message, index) in mock" :key="index">
+        {{ message }}
+      </div>
+    </div>
+    <!-- Users-->
+    <div class="col-span-1 row-span-6 border-1 border-purple-500">
+      <div class="p-2 flex flex-col">
+        <p>Beyondu</p>
+        <p>Whatever</p>
+      </div>
+    </div>
+    <!-- Input -->
+    <div class="col-span-5 border-2 border-green-700">
+      <div class="h-full w-[100%] flex flex-row">
+        <form class="w-[90%] border-1 border-orange-600">
+          <input class="w-full h-full" v-model="text" placeholder="type a message" />
+        </form>
+        <!-- button submit -->
+        <div class="w-[10%] border-1 border-white">Submit</div>
+      </div>
+    </div>
+  </main>
+</template>
